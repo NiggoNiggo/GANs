ff744e4dad37dbbcec71d66e8ebfde289a1454cf
Nico Becker <nicolas1998.becker@gmail.com>
2024-08-29 09:07:32 +0200

    added sample length of 32768 samples

--- WaveGAN/wave_generator.py
+++ WaveGAN/wave_generator.py
@@ -3,19 +3,32 @@ from torch import nn
 
 class WaveGenerator(nn.Module):
     def __init__(self,
-                 num_layers,
-                 c,
-                 d):
+                 len_samples:int,
+                 num_layers:int,
+                 c:int,
+                 d:int):
         super().__init__()
         self.num_layers = num_layers
+        self.len_samples = len_samples
         self.d = d
         self.c = c
-        in_channels = [16,8,4,2,1]
-        out_channels = [8,4,2,1,self.c]
-        layers = [nn.Linear(100,256*self.d), 
-                    nn.Unflatten(1, (16*self.d, 16)),
-                    nn.ReLU(True)
-                    ]
+        # out_channels = [16,8,4,2,1,self.c]
+
+
+        if self.len_samples  == 16384:
+            in_channels = [ 16// (2**i) for i in range(self.num_layers)]
+            out_channels = [16 // (2**i) for i in range(self.num_layers-1)] + [self.c]
+            layers = [nn.Linear(100,256*self.d), 
+                        nn.Unflatten(1, (16*self.d, 16)),
+                        nn.ReLU(True)
+                        ]
+        elif self.len_samples == 32768:
+            in_channels = [ 32// (2**i) for i in range(self.num_layers)]
+            out_channels = [32 // (2**i) for i in range(self.num_layers-1)] + [self.c]
+            layers = [nn.Linear(100,256*self.d), 
+                        nn.Unflatten(1, (32*self.d, 32)),
+                        nn.ReLU(True)
+                        ]
 
         for num in range(self.num_layers):
             current_layer = UpscaleConvTranspose1d(in_channels=in_channels[num]*self.d,
