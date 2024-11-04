080c1bf80bdab0f1da9b727ce7e088e9f9f28238
Nico Becker <nicolas1998.becker@gmail.com>
2024-11-04 08:49:00 +0100

    updatet wavegan main file

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ try:
     args = parse_gan_args(current_gan,remaining_args)
     print(f"The following Arguments are loaded for the GAN:{args}")
 except:
-    current_gan = "conditional_wavegan"
+    current_gan = "wavegan"
     args = parse_gan_args(current_gan,[])
     print(f"No Gan type selected. Start Training {current_gan}")
 
@@ -55,7 +55,7 @@ if __name__ == "__main__":
                         name="specgan_drums_II",
                         params=args)
         name_gen, name_disc = repr(specgan.gen), repr(specgan.disc)
-        specgan.load_models(name_gen=specgan.gen,name_disc=specgan.disc)
+        # specgan.load_models(name_gen=specgan.gen,name_disc=specgan.disc)
         
         specgan.make_entire_training()
 
@@ -63,17 +63,23 @@ if __name__ == "__main__":
 
 
     
-
+    #bestes model übher nicht über die epoche suchen sondern über den fid score im namen 
+    #
+    #
+    #
 
     elif current_gan == "wavegan":
         
         wavegan = WaveGAN(
                         device="cuda",
-                        name="conditional_1s",
+                        name="normal_new_test",
+                        # name="low_params",
                         params=args
                         )
         name_gen, name_disc = repr(wavegan.gen), repr(wavegan.disc)
         wavegan.load_models(name_gen=wavegan.gen,name_disc=wavegan.disc)
+        # wavegan.load_models(name_disc=r"H:\Results\GANS\normal_new_test\models\Discriminator_WaveGAN_epoch_246_fid_86.44.pth",
+        #                     name_gen=r"H:\Results\GANS\normal_new_test\models\Generator_WaveGAN_epoch_246_fid_86.44.pth")
         wavegan.make_entire_training()
     
     elif current_gan == "conditional_wavegan":
@@ -82,17 +88,17 @@ if __name__ == "__main__":
                                      device="cuda",
                                      name="testing")
         
-        data = {
-            "lr_g":[1e-4,1e-5,1e-6,2e-5],
-            "lr_d":[1e-4,1e-5,1e-6,2e-5],
-            "batchsize":[32,64,128],
-            "n_crit":list(range(1,10)),
-            "lam":list(range(1,15)),
-             "d":[1,8,16]
-            }    
-        tuner = RandomSearch("Optimized_GAN",10,data,args,8,ConditionalWaveGAN)
-        tuner.tune_params(device="cuda")
-        results = tuner.check_conditions(r"results_hypertuning.csv")
+        # data = {
+        #     "lr_g":[1e-4,1e-5,1e-6,2e-5],
+        #     "lr_d":[1e-4,1e-5,1e-6,2e-5],
+        #     "batchsize":[32,64,128],
+        #     "n_crit":list(range(1,10)),
+        #     "lam":list(range(1,15)),
+        #      "d":[1,8,16]
+        #     }    
+        # tuner = RandomSearch("Optimized_GAN",10,data,args,8,ConditionalWaveGAN)
+        # tuner.tune_params(device="cuda")
+        # results = tuner.check_conditions(r"results_hypertuning.csv")
 
         # tuner = RandomSearch(2,data,args,2,ConditionalWaveGAN)
         # tuner.tune_params(name="randomSearchTest",
@@ -104,11 +110,14 @@ if __name__ == "__main__":
                                     #  name="test_noe_new")
         # print(results)
 ###-----------------
-        # name_gen, name_disc = repr(wavegan.gen), repr(wavegan.disc)
-        # wavegan.load_models(name_gen=wavegan.gen,name_disc=wavegan.disc)
+        name_gen, name_disc = repr(wavegan.gen), repr(wavegan.disc)
+        wavegan.load_models(name_gen=wavegan.gen,name_disc=wavegan.disc)
         wavegan.make_entire_training()
         # wavegan.valid_afterwards(r"F:\new_models",mk_audio=True)
         
     #fehlt nur noch loss function plotten und fid plotten mit kwargs
     #early stop bedingung
     #dann tensorboard
+
+
+    #dannach dann mal mit batchnorm True beides laufen lassenm
